dd815de878703aba2f8ab3fdc940ad3424296dc9
valgarf <valgarf@posteo.de>
2023-06-18 13:56:44 +0200

    tasks fix

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,8 +1,10 @@
 from invoke import Collection
 
-from . import docker
+from . import docker, local, publish
 
 ns = Collection()
 ns.add_collection(Collection.from_module(docker))
+ns.add_collection(Collection.from_module(local))
+ns.add_collection(Collection.from_module(publish))
 
 ns.configure({'docker': { 'target': {'build': 'all', 'run': 'debug'}}})
